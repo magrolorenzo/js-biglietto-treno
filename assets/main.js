8978f8962eca498552ac93ee4bdde07fb5e58e10
Lorenzo Magro <magrolorenzo@gmail.com>
2020-10-13 15:31:12 +0200

    Creazione files

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+//Il programma deve chiedere all'utente il numero di km da percorrere e la sua età.
+
+// il prezzo del biglietto dipende dalla distanza da percorrere e costa € 0.21 / km
+// è previsto uno sconto del 20% per il minorenni
+// gli over 65 hanno diritto ad uno sconto del 40%
